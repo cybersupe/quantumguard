6549cf8473edf2b3eb000e31e6ac4b137330084c
Thisi <thisispayyavula@gmail.com>
2026-04-21 17:34:41 -0500

    improve detection - add ECB, weak TLS, RC4, DES, hardcoded secrets

--- scanner/patterns.py
+++ scanner/patterns.py
@@ -5,6 +5,11 @@ VULNERABLE_PATTERNS = {
         "patterns": [
             "RSA", "rsa", "RSAKey", "RSA.new",
             "RSA.generate", "PKCS1_OAEP", "PKCS1_v1_5",
+            "RSA.import_key", "RSA.export_key",
+            "generateKeyPairSync.*rsa",
+            "KeyPairGenerator.*RSA",
+            "RSAPublicKeySpec", "RSAPrivateKeySpec",
+            "forge.pki.rsa", "NodeRSA",
         ]
     },
     "ECC": {
@@ -13,6 +18,9 @@ VULNERABLE_PATTERNS = {
         "patterns": [
             "EC", "ECC", "ECDSA", "ECDH",
             "elliptic", "secp256k1", "prime256v1",
+            "createECDH", "generateKeyPairSync.*ec",
+            "KeyPairGenerator.*EC", "ECPublicKeySpec",
+            "brainpool", "curve25519",
         ]
     },
     "DH": {
@@ -20,7 +28,9 @@ VULNERABLE_PATTERNS = {
         "replacement": "CRYSTALS-Kyber",
         "patterns": [
             "DH", "DHE", "DiffieHellman",
-            "diffie-hellman",
+            "diffie-hellman", "getDiffieHellman",
+            "createDiffieHellman", "KeyPairGenerator.*DH",
+            "DHPublicKeySpec",
         ]
     },
     "DSA": {
@@ -28,6 +38,8 @@ VULNERABLE_PATTERNS = {
         "replacement": "CRYSTALS-Dilithium",
         "patterns": [
             "DSA", "DSAKey", "DSS",
+            "KeyPairGenerator.*DSA",
+            "generateKeyPairSync.*dsa",
         ]
     },
     "MD5": {
@@ -35,6 +47,8 @@ VULNERABLE_PATTERNS = {
         "replacement": "SHA-3 or SPHINCS+",
         "patterns": [
             "MD5", "md5", "hashlib.md5",
+            "createHash.*md5", "DigestUtils.md5",
+            "MessageDigest.*MD5",
         ]
     },
     "SHA1": {
@@ -42,6 +56,53 @@ VULNERABLE_PATTERNS = {
         "replacement": "SHA-3 or SPHINCS+",
         "patterns": [
             "SHA1", "sha1", "hashlib.sha1",
+            "createHash.*sha1", "DigestUtils.sha1",
+            "MessageDigest.*SHA-1",
+        ]
+    },
+    "HARDCODED_SECRET": {
+        "severity": "HIGH",
+        "replacement": "Use environment variables or secret manager",
+        "patterns": [
+            "password.*=.*['\"]\\w+['\"]",
+            "secret.*=.*['\"]\\w+['\"]",
+            "api_key.*=.*['\"]\\w+['\"]",
+            "private_key.*=.*['\"]\\w+['\"]",
+            "SECRET_KEY.*=.*['\"]\\w+['\"]",
+        ]
+    },
+    "ECB_MODE": {
+        "severity": "HIGH",
+        "replacement": "AES-GCM or ChaCha20-Poly1305",
+        "patterns": [
+            "AES.MODE_ECB", "Cipher.MODE_ECB",
+            "ECB", "mode.*ecb", "ecb.*mode",
+        ]
+    },
+    "WEAK_TLS": {
+        "severity": "HIGH",
+        "replacement": "TLS 1.3",
+        "patterns": [
+            "SSLv2", "SSLv3", "TLSv1", "TLSv1.1",
+            "ssl.PROTOCOL_SSLv2", "ssl.PROTOCOL_SSLv3",
+            "ssl.PROTOCOL_TLSv1",
+            "PROTOCOL_TLS", "TLS_RSA",
+        ]
+    },
+    "RC4": {
+        "severity": "CRITICAL",
+        "replacement": "AES-256-GCM",
+        "patterns": [
+            "RC4", "rc4", "ARC4", "arc4",
+            "Cipher.*RC4",
+        ]
+    },
+    "DES": {
+        "severity": "CRITICAL",
+        "replacement": "AES-256-GCM",
+        "patterns": [
+            "DES", "3DES", "TripleDES", "DESede",
+            "Cipher.*DES", "des.*mode",
         ]
     },
 }
@@ -52,6 +113,10 @@ JS_PATTERNS = {
     "DH": ["crypto.getDiffieHellman", "crypto.createDiffieHellman"],
     "MD5": ["crypto.createHash.*md5", "md5("],
     "SHA1": ["crypto.createHash.*sha1", "sha1("],
+    "ECB_MODE": ["AES.MODE_ECB", "mode.*ecb"],
+    "WEAK_TLS": ["SSLv2", "SSLv3", "TLSv1"],
+    "RC4": ["RC4", "ARC4"],
+    "DES": ["DES", "3DES", "TripleDES"],
 }
 
 TS_PATTERNS = {
@@ -60,6 +125,10 @@ TS_PATTERNS = {
     "DH": ["getDiffieHellman", "createDiffieHellman"],
     "MD5": ["createHash.*md5", "md5("],
     "SHA1": ["createHash.*sha1", "sha1("],
+    "ECB_MODE": ["AES.MODE_ECB", "mode.*ecb"],
+    "WEAK_TLS": ["SSLv2", "SSLv3", "TLSv1"],
+    "RC4": ["RC4", "ARC4"],
+    "DES": ["DES", "3DES", "TripleDES"],
 }
 
 JAVA_PATTERNS = {
@@ -68,6 +137,9 @@ JAVA_PATTERNS = {
     "DH": ["KeyPairGenerator.*DH", "DHPublicKeySpec", "javax.crypto.interfaces.DHKey"],
     "MD5": ["MessageDigest.*MD5", "DigestUtils.md5"],
     "SHA1": ["MessageDigest.*SHA-1", "DigestUtils.sha1"],
+    "ECB_MODE": ["Cipher.MODE_ECB", "AES/ECB"],
+    "WEAK_TLS": ["SSLv2", "SSLv3", "TLSv1"],
+    "DES": ["DESede", "DES", "Cipher.*DES"],
 }
 
 SEVERITY_SCORE = {
