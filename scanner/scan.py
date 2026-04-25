31d41344dccfd786fcccf13303a90af5e702f6a1
cybersupe <163059356+cybersupe@users.noreply.github.com>
2026-04-25 17:12:08 -0500

    Update scan.py

--- scanner/scan.py
+++ scanner/scan.py
@@ -29,7 +29,7 @@ def scan_file(filepath):
 
 def scan_directory(directory):
     all_findings = []
-    supported = (".py", ".js", ".java", ".ts", ".go", ".rs")
+    supported = (".py", ".js", ".java", ".ts", ".go", ".rs", ".c", ".cpp", ".cc", ".h", ".hpp")
     for root, dirs, files in os.walk(directory):
         dirs[:] = [d for d in dirs if d not in ["venv", "node_modules", ".git", "__pycache__"]]
         for file in files:
