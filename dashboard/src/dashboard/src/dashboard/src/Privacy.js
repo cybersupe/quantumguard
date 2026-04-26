857deaf3c829d44b3fd6d3e758a4f910edc8edf7
cybersupe <163059356+cybersupe@users.noreply.github.com>
2026-04-26 14:56:47 -0500

    Create Privacy.js

---
+++ dashboard/src/dashboard/src/dashboard/src/Privacy.js
@@ -0,0 +1,10 @@
+export default function Privacy() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Privacy Policy</h1>
+      <p>We do not permanently store any source code.</p>
+      <p>Data is used only for analysis and deleted after scanning.</p>
+      <p>We do not share user data with third parties.</p>
+    </div>
+  );
+}
